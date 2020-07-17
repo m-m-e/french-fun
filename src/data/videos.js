ec7fece0b41e95eaa4a2730de622d9a5b9da75c0
Megan Edwards <megan.edwards@osoco.es>
2020-07-17 18:21:08 +0200

    Add scss and list of videos with metadata

---
+++ src/data/videos.js
@@ -0,0 +1,30 @@
+export const videosList = [
+    {
+      "id": 0,
+      "title": "Numbers 1-10 song",
+      "src": "https://www.youtube.com/embed/I2ySRLDewVM",
+      "type": "song",
+      "tags": ["numbers"]
+    },
+    {
+      "id": 1,
+      "title": "Bonjour mes amis",
+      "src": "https://www.youtube.com/embed/eKkwdc2YZY8",
+      "type": "song",
+      "tags": ["greetings", "actions"]
+    },
+    {
+      "id": 2,
+      "title": "Tourne tourne petit moulin",
+      "src": "https://www.youtube.com/embed/kwqsOy9hS2U",
+      "type": "song",
+      "tags": ["actions"]
+    },
+    {
+      "id": 3,
+      "title": "Numbers 1-10",
+      "src": "https://www.youtube.com/embed/388iv0rOzpI",
+      "type": "lesson",
+      "tags": ["numbers", "counting"]
+    }
+  ]
--- src/home/index.js
+++ src/home/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Header from '../header';
 import Footer from '../footer';
-import './styles.css';
+import Videos from '../videos';
+import './styles.scss';
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
       <main className="main_content">
-        <section id="welcome">
+        <section id="welcome" class="welcome-content">
           <h1 className="main_title">Welcome to Reuben's French Site!</h1>
+          <p>Do you want to learn some French?</p>
+          <p>On this site you'll find some fun videos and resources to help you</p>
+          <p>Let's go!</p>
         </section>
         <section id="videos">
-          <h2 className="main_subtitle">Videos</h2>
-          <div className="videos-container">
-            <iframe title="video-1" src="https://player.vimeo.com/video/437959757" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-          </div>
+          <Videos />
         </section>
         <section id="resources">
           <h2 className="main_subtitle">Resources</h2>
