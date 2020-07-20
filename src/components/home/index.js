35b2a5eaa25792ea16b9c7da794fe49384105d73
Megan Edwards <megan.edwards@osoco.es>
2020-07-20 22:02:24 +0200

    Add mobile styles

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,15 +3,16 @@ import './styles.scss';
 import map from '../../images/UK-and-Europe.png'
 
 const Home = () => {
+  const width = window.innerWidth;
   return (
     <div className="home">
       <section className="welcome-content">
         <h1 className="main_title">Welcome to Reuben's French Site!</h1>
-        <div className="maps">
+        {(width > 768) && <div className="maps">
           <p className="nottingham">Nottingham</p>
           <div className="arrow">France →</div>
           <img src={map} alt="Map of UK and France with Nottingham highlighted"/>
-        </div>
+        </div>}
 
         <p>Learning languages is fun!</p>
         <p>On this site you'll find some fun videos and resources to help you learn some French.</p>
